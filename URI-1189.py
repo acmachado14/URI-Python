fd808ce9e513d910a1e8cc72fdd141b6728246c6
Angelo Machado <cupertinoangelo13@gmail.com>
2021-09-14 16:43:09 -0300

    URI-1189

---
+++ URI-1189.py
@@ -0,0 +1,29 @@
+M = [[0 for j in range(12)]for i in range(12)]
+
+O = input()
+
+for i in range (len(M)):
+    for j in range (len(M[0])):
+        M[i][j] = float(input())
+
+cont = 0
+col = 1
+soma = 0.0
+for i in range(1,11):
+    for j in range(0,col):
+        soma += M[i][j]
+       
+        cont += 1
+    if i < 5:
+        col += 1
+    if i > 5:
+        col -= 1
+   
+   
+
+med = soma / cont
+
+if O == "S":      
+    print("%.1f"%soma)      
+elif O == "M":
+    print("%.1f"%(soma/12))
